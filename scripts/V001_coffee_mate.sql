2b0a5b876e8dce1e5ddbb8ea3354bf50acb84aa2
Anton Morozov <flow1917@gmail.com>
2024-03-31 14:30:19 +1100

    Update V001_coffee_mate.sql
    Added two tables for holding reviews and cafes data gathered from google api

--- scripts/V001_coffee_mate.sql
+++ scripts/V001_coffee_mate.sql
@@ -38,3 +38,30 @@ predicitonValue DECIMAL(3, 2),
 predictionTimeStamp varchar(255),
 PRIMARY KEY (predictionIndex)
 );
+
+-- This table holds cafes's params
+CREATE TABLE cafes(
+cafeIndex INT AUTO_INCREMENT,
+cafeId varchar(255),
+displayName varchar(255),
+formattedAddress varchar(255),
+latitude varchar(255),
+longitude varchar(255),
+servesCoffee varchar(255),
+takeout varchar(255),
+goodForChildren varchar(255),
+goodForGroups varchar(255),
+PRIMARY KEY (cafeIndex)
+);
+
+
+-- This table holds reviews
+CREATE TABLE reviews(
+reviewIndex INT AUTO_INCREMENT,
+cafeId varchar(255),
+userFirstName varchar(255),
+userSecondName varchar(255),
+ranking  int(1),
+PRIMARY KEY (cafeIndex)
+);
+
