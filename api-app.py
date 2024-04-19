f40fd0b483d2430ce1308a6677b1dcaca74312a1
Anton Morozov <flow1917@gmail.com>
2024-04-20 01:39:54 +1000

    Update api-app.py

--- api-app.py
+++ api-app.py
@@ -1,7 +1,6 @@
 import json
 from flask import Flask, request, jsonify, render_template
 from flask_swagger_ui import get_swaggerui_blueprint
-import mysql.connector
 
 SWAGGER_URL="/swagger"
 API_URL="/static/swagger.json"
@@ -25,28 +24,76 @@ def index():
 
 @app.route('/users/<id>', methods=['GET'])
 def get_users(id):
-    return jsonify({'postcode': '2515', 'gender': 0, 'age': 45})
+    match id:
+        case '':
+            return f"Success", 200
+        case '01061add-1302-4846-bb8e-b8e0ffe7ac84':
+            return jsonify({'postcode': '2552', 'gender': 1, 'age': 22}), 200
+        case '25c26f74-d0eb-408b-8f97-26429032c832':
+            return jsonify({'postcode': '2500', 'gender': 0, 'age': 0}), 200
+        case '273cf928-a41f-43c6-9dac-c13385b2a29e':
+            return jsonify({'postcode': '2518', 'gender': 1, 'age': 32}), 200
+    return f"Not found", 404
 
 @app.route('/users/<id>', methods=['PUT'])
 def put_users(id):
-    return f"Success", 200
+    match id:
+        case '':
+            return f'Success', 200
+        case '01061add-1302-4846-bb8e-b8e0ffe7ac84':
+            return f"Conflict", 409
+        case '25c26f74-d0eb-408b-8f97-26429032c832':
+            return f"Conflict", 409
+        case '273cf928-a41f-43c6-9dac-c13385b2a29e':
+            return f"Conflict", 409
+    return f"Created", 201
 
 @app.route('/users/<id>/recommendations', methods=['GET'])
 def get_users_recommendations(id):
-    return jsonify({'predictions': [{'cafe': 'a6ffc3e9-a33b-4afb-b9c8-aa63bd0e49f3', 'ranking': 0.98}, {'cafe': '6901e116-3bde-44bc-9b67-9b0a7b31e3a5', 'ranking': 0.94}, {'cafe': '7259acf8-90bc-40b5-98cd-73781536dcbe', 'ranking': 0.82}]})
+    match id:
+        case '':
+            return f"Success", 200
+        case '01061add-1302-4846-bb8e-b8e0ffe7ac84':
+            return jsonify({
+                'predictions': 
+                [{'cafe': 'ChIJvxY0e1UZE2sRshBd4u2VPNc', 'ranking': 0.98},
+                 {'cafe': 'ChIJEeMz8sMZE2sRynqdMW4YF9g', 'ranking': 0.94}, 
+                 {'cafe': 'ChIJj8l_uYYUE2sRPddj46-ukO0', 'ranking': 0.82}]}), 200
+        case '25c26f74-d0eb-408b-8f97-26429032c832':
+            return jsonify({
+                'predictions': 
+                [{'cafe': 'ChIJvxY0e1UZE2sRshBd4u2VPNc', 'ranking': 0.98},
+                 {'cafe': 'ChIJEeMz8sMZE2sRynqdMW4YF9g', 'ranking': 0.94}, 
+                 {'cafe': 'ChIJj8l_uYYUE2sRPddj46-ukO0', 'ranking': 0.82}]}), 200
+        case '273cf928-a41f-43c6-9dac-c13385b2a29e':
+            return jsonify({
+                'predictions': 
+                [{'cafe': 'ChIJvxY0e1UZE2sRshBd4u2VPNc', 'ranking': 0.98},
+                 {'cafe': 'ChIJEeMz8sMZE2sRynqdMW4YF9g', 'ranking': 0.94}, 
+                 {'cafe': 'ChIJj8l_uYYUE2sRPddj46-ukO0', 'ranking': 0.82}]}), 200
+    return f"Not found", 404
 
 @app.route('/users/<id>/responses', methods=['PUT'])
 def put_responses(id):
-    return f"Success", 200
+    match id:
+        case '':
+            return f'Success'
+        case '97d201a6-e41f-46b7-a4f3-71030179249a':
+            return f'Not found', 404
+        case 'da86dd4c-32db-4591-9915-6712f92d2bbc':
+            return f'Not found', 404
+    return f'Created', 201
 
 @app.route('/users/<id>/rankings/<cafeid>', methods=['PUT'])
 def put_ranking(id, cafeid):
-    return f"Success", 200
-
-@app.route('/db', methods=['GET'])
-def db_test():
-    #foo = print(mydb)
-    return f'{mydb}', 200
+    match id:
+        case '':
+            return f'Success', 200
+        case '97d201a6-e41f-46b7-a4f3-71030179249a':
+            return f'Not found', 404
+        case 'da86dd4c-32db-4591-9915-6712f92d2bbc':
+            return f'Not found', 404
+    return f'Created', 201
 
 #Run swagger 
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
