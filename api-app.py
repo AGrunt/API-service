8840bb1ba454780d2f69db22a601196792300af7
Anton Morozov <flow1917@gmail.com>
2024-04-20 01:56:59 +1000

    Update api-app.py

--- api-app.py
+++ api-app.py
@@ -72,28 +72,32 @@ def get_users_recommendations(id):
                  {'cafe': 'ChIJEeMz8sMZE2sRynqdMW4YF9g', 'ranking': 0.94}, 
                  {'cafe': 'ChIJj8l_uYYUE2sRPddj46-ukO0', 'ranking': 0.82}]}), 200
     return f"Not found", 404
-
+        
 @app.route('/users/<id>/responses', methods=['PUT'])
 def put_responses(id):
     match id:
         case '':
-            return f'Success'
-        case '97d201a6-e41f-46b7-a4f3-71030179249a':
-            return f'Not found', 404
-        case 'da86dd4c-32db-4591-9915-6712f92d2bbc':
-            return f'Not found', 404
-    return f'Created', 201
+            return f'Success', 200
+        case '01061add-1302-4846-bb8e-b8e0ffe7ac84':
+            return f'Created', 201
+        case '25c26f74-d0eb-408b-8f97-26429032c832':
+            return f'Created', 201        
+        case '273cf928-a41f-43c6-9dac-c13385b2a29e':
+            return f'Created', 201
+    return f'Not found', 404
 
 @app.route('/users/<id>/rankings/<cafeid>', methods=['PUT'])
 def put_ranking(id, cafeid):
     match id:
         case '':
             return f'Success', 200
-        case '97d201a6-e41f-46b7-a4f3-71030179249a':
-            return f'Not found', 404
-        case 'da86dd4c-32db-4591-9915-6712f92d2bbc':
-            return f'Not found', 404
-    return f'Created', 201
+        case '01061add-1302-4846-bb8e-b8e0ffe7ac84':
+            return f'Created', 201
+        case '25c26f74-d0eb-408b-8f97-26429032c832':
+            return f'Created', 201        
+        case '273cf928-a41f-43c6-9dac-c13385b2a29e':
+            return f'Created', 201
+    return f'Not found', 404
 
 #Run swagger 
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
