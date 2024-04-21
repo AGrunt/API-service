9fd1e25b37cbe483b1744253c3dacccef672c25b
Anton Morozov <flow1917@gmail.com>
2024-04-22 00:06:08 +1000

    Update api-app.py
    completed a lot of things. some endpoint functions ,retrieving and inserting functions for request data.
    still in progress:
    * get_users_recomendastions
    * put_responces

--- api-app.py
+++ api-app.py
@@ -1,6 +1,20 @@
 import json
 from flask import Flask, request, jsonify, render_template
 from flask_swagger_ui import get_swaggerui_blueprint
+import mysql.connector
+from datetime import datetime
+
+#Creating db connector
+dbConnection = mysql.connector.connect(
+  host="api-db",
+  port="3306",
+  user="sample",
+  password="sample",
+  database="coffee-mate"
+  )
+
+# Make a cursor
+cursor = dbConnection.cursor()
 
 SWAGGER_URL="/swagger"
 API_URL="/static/swagger.json"
@@ -14,9 +28,67 @@ swagger_ui_blueprint = get_swaggerui_blueprint(
 )
 
 # Make a cursor
-#cursor = mydb.cursor()
+cursor = dbConnection.cursor()
 
 app = Flask(__name__)
+
+#Function for retrieving and checking existence of data in the database. Return true if data found.
+def get_data(table, data, check):
+    #Expected that data is a dictionarry.  with {columnName:value} format
+    conditions_string = ''
+    values = ()
+    #combine data
+    for index, column in enumerate(data.keys()):
+        if index < len(data.keys()) - 1:
+            conditions_string +=  f'{column} = %s AND '
+        else:
+            conditions_string +=  f'{column} = %s'
+        values += tuple([data[column]])
+    #make select stetment
+    if check:
+        check_stmt = (
+            f'SELECT COUNT(*) FROM {table} '
+            f'WHERE {conditions_string}'
+            )
+        cursor.execute(check_stmt, values)
+        myresult = cursor.fetchone()
+        if int(myresult[0]) > 0:
+            return True
+    else:
+        select_stmt = (
+            f'SELECT * FROM {table} '
+            f'WHERE {conditions_string}'
+            )
+        cursor.execute(select_stmt, values)
+        result = cursor.fetchall()
+        print(result)
+        return result
+    
+def put_data(table, data):
+#Expected that data is a dictionarry.  with {columnName:value} format
+    columns_string = ''
+    values_string = ''
+    values = ()
+    #combine data
+    try: 
+        for index, column in enumerate(data.keys()):
+            if index < len(data.keys()) - 1:
+                columns_string += f'{column}, '
+                values_string +=  f'%s, '
+            else:
+                columns_string += f'{column}'
+                values_string +=  f'%s'
+            values += tuple([data[column]])
+        #make insert stetement
+        insert_stmt = (
+        f'INSERT INTO {table} ({columns_string}) '
+        f'VALUES ({values_string})'
+        )
+        cursor.execute(insert_stmt, values)
+        dbConnection.commit()
+    except Exception as err:
+        return False, err, 500
+    return True
 
 @app.route('/')
 def index():
@@ -24,29 +96,47 @@ def index():
 
 @app.route('/users/<id>', methods=['GET'])
 def get_users(id):
+    # combine data
+    table = 'usersTable'
+    data = {'userid': str(id)}    
     match id:
         case '':
-            return f"Success", 200
-        case '01061add-1302-4846-bb8e-b8e0ffe7ac84':
-            return jsonify({'postcode': '2552', 'gender': 1, 'age': 22}), 200
-        case '25c26f74-d0eb-408b-8f97-26429032c832':
-            return jsonify({'postcode': '2500', 'gender': 0, 'age': 0}), 200
-        case '273cf928-a41f-43c6-9dac-c13385b2a29e':
-            return jsonify({'postcode': '2518', 'gender': 1, 'age': 32}), 200
-    return f"Not found", 404
+            return f'Success', 200
+    
+    if not get_data(table, data, check=True):
+        return f"Not found", 404
+    result = get_data(table, data, check=False)
+    # Creation output dictionary 
+    output = {       
+        'gender': result[0][1],
+        'age': result[0][2],
+        'postcode': result[0][4]
+        }
+    return jsonify(output), 200
 
 @app.route('/users/<id>', methods=['PUT'])
 def put_users(id):
     match id:
         case '':
             return f'Success', 200
-        case '01061add-1302-4846-bb8e-b8e0ffe7ac84':
-            return f"Conflict", 409
-        case '25c26f74-d0eb-408b-8f97-26429032c832':
-            return f"Conflict", 409
-        case '273cf928-a41f-43c6-9dac-c13385b2a29e':
-            return f"Conflict", 409
-    return f"Created", 201
+    #Check if user exists already
+    table = 'usersTable'
+    data = {'userid': id}
+    if get_data(table, data, check=True):
+        return f"Conflict", 409
+    #Put user's data into database
+    #combine data
+    data = {
+        'userId': str(id),
+        'gender': int(request.json['gender']),
+        'age': int(request.json['age']),
+        'registrationTimeStamp': str(datetime.now()),
+        'postcode': str(request.json['postcode'])
+        }
+    results = put_data('usersTable', data)
+    if type(results) is not bool:
+        return f'Something went wrong. Data :<{data}>. Results: <{results}>', 500
+    return f'Created.', 201
 
 @app.route('/users/<id>/recommendations', methods=['GET'])
 def get_users_recommendations(id):
@@ -88,16 +178,46 @@ def put_responses(id):
 
 @app.route('/users/<id>/rankings/<cafeid>', methods=['PUT'])
 def put_ranking(id, cafeid):
-    match id:
-        case '':
-            return f'Success', 200
-        case '01061add-1302-4846-bb8e-b8e0ffe7ac84':
-            return f'Created', 201
-        case '25c26f74-d0eb-408b-8f97-26429032c832':
-            return f'Created', 201        
-        case '273cf928-a41f-43c6-9dac-c13385b2a29e':
-            return f'Created', 201
-    return f'Not found', 404
+    #check if user exist and cafeid exist
+    #Check userId
+    table = 'usersTable'
+    data = {'userId': id}
+    if not get_data(table, data, check=True):
+         return f'<userId>: <{id}>. Not found ', 404
+    #Check cafeId
+    table = 'cafes'
+    data = {'cafeId': cafeid}
+    if not get_data(table, data, check=True):
+         return f'<cafeid>: <{cafeid}>. Not found', 404
+    
+    #insert user ranking data intop database
+    table = 'rankings'
+    data = {
+        'userId': id,
+        'cafeId': cafeid, 
+        'categoryId': request.json['rankings'][0]['categoryId'],
+        'rankingValue': request.json['rankings'][0]['rank'],
+        'rankingTimeStamp': datetime.now()
+        }
+    #Generating console output
+    results = put_data(table, data)
+    if type(results) is not bool:
+        return f'Something went wrong. Data :<{data}>. Results: <{results}>', 500
+    return f'Created', 201
+
+@app.route('/db_test')
+def bd_test():
+    #TEst database connection
+    select_stmt = (
+        f'SHOW TABLES'
+        )
+    cursor.execute(select_stmt)
+    dbs = ''
+    for db in cursor.fetchall():
+        dbs += f'{db}, '
+    
+    
+    return dbs
 
 #Run swagger 
 app.register_blueprint(swagger_ui_blueprint, url_prefix=SWAGGER_URL)
